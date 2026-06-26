92e19a77f13bf500c3b004e2c3a1cc32a9304bdb
wei1kuan <189067884@qq.com>
2026-06-27 01:43:42 +0800

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/wenlv-college/',
+  base: './',
   server: {
     port: 5173,
     strictPort: true,
